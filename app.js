47be4a31702f62ce0e3938e9a607ed88e935c98d
Lau Zhuxian <swisslzx@hotmail.com>
2021-05-08 18:47:26 +0800

    Level 4 Security: Hash & Salt with bcrypt

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 //jshint esversion:6
 
-//--- L3 Security Setup ---//
-// L3 security setup includes further secures by hashing with md5
-// npm package to install: md5
+//--- L4 Security Setup ---//
+// L4 security setup includes using bcrypt to salt and hash
+// npm package to install: bycrypt
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
-console.log("weak password hash: " + md5("123456"));
-console.log("strong password hash: " + md5("sdkfjalskdf3453jsdslk4"));
+
 
 // setting up ejs
 app.use(express.static("public"));
@@ -51,34 +51,36 @@ app.get('/register', function(req,res){
 });
 
 app.post('/register', function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
 
-  newUser.save(function(err){
-    if (err) {
-      console.log(err);
-    } else {
-      res.render('secrets');
-    }
+    newUser.save(function(err){
+      if (err) {
+        console.log(err);
+      } else {
+        res.render('secrets');
+      }
+    });
   });
 });
 
 app.post('/login', function(req, res){
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email: username}, function(err, foundUser){
     if (err) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password){
-          res.render('secrets');
-        } else {
-          res.redirect('/');
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if(result === true) {
+            res.render('secrets');
+          }
+        });
       }
     }
   });
